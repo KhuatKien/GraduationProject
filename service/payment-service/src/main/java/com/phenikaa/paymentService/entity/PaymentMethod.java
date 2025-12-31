da280b784d833f2536cccfb09a5986f608dd2691
KhuatKien <91423106+KhuatKien@users.noreply.github.com>
2025-12-31 15:40:55 +0700

    Delete service/payment-service directory

--- service/payment-service/src/main/java/com/phenikaa/paymentService/entity/PaymentMethod.java
+++
@@ -1,5 +0,0 @@
-package com.phenikaa.paymentService.entity;
-
-public enum PaymentMethod {
-    MOMO, ZALOPAY, BANK_TRANSFER, CREDIT_CARD, QR_CODE
-}
--- service/payment-service/src/main/java/com/phenikaa/paymentService/entity/PaymentStatus.java
+++
@@ -1,5 +0,0 @@
-package com.phenikaa.paymentService.entity;
-
-public enum PaymentStatus {
-    COMPLETED, FAILED
-}
